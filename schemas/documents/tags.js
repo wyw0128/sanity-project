5a424b3493b2e12c354e0ef9bf0288713532485c
wyw0128 <yiweiwu0128@gmail.com>
2023-03-11 16:20:34 +1100

    add tags page to schema

--- schemas/documents/tags.js
+++ schemas/documents/tags.js
@@ -1,6 +1,24 @@
+import client from "part:@sanity/base/client";
+
 export default {
-  title: "Tags",
-  name: "tags",
   type: "document",
+  name: "tags",
+  title: "Tags",
+  // liveEdit: true,
+  validation: (Rule) =>
+    Rule.custom(async (fields) => {
+      const tagData = await client.fetch(`*[_type == "tags"]`);
+      const tagArray = tagData.map((tag) => tag.tag);
+      for (let i = 0; i < tagArray.length; i++) {
+        if (
+          tagArray[i] === fields.tag &&
+          !tagData[i]._id.startsWith("drafts.")
+        ) {
+          console.log(tagArray[i], fields.tag);
+          return "This tag already exists";
+        }
+      }
+      return true;
+    }),
   fields: [{ type: "string", name: "tag" }],
 };
